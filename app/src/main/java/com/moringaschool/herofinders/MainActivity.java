b87d9a00ac28e1a829fe0141cc9101f23a7a5358
edithntinyari <emothegi@gmail.com>
2020-11-13 13:18:43 +0300

    First commit

---
+++ app/src/main/java/com/moringaschool/herofinders/MainActivity.java
@@ -0,0 +1,43 @@
+package com.moringaschool.herofinders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity  implements  View.OnClickListener {
+    @BindView(R.id.findHerosfinderButton)
+    Button mFindWildlifesButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindWildlifesButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindWildlifesButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, HerosActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+
+}
