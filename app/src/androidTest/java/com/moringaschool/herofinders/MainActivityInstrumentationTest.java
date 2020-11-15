7e2e565172379a4dab295e21fd1fad16ad936c31
edithntinyari <emothegi@gmail.com>
2020-11-15 22:52:57 +0300

    added MainActivityInstrumentationTest.java

--- app/src/androidTest/java/com/moringaschool/herofinders/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/herofinders/MainActivityInstrumentationTest.java
@@ -1,4 +1,11 @@
 package com.moringaschool.herofinders;
 
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
 public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
 }
